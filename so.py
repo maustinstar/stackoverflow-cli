51c3a2b58e250df6fc239e16b0cee2ad48780c30
maustinstar <maustinstar@gmail.com>
2020-04-07 16:28:40 -0500

    use argparse; add -l

--- so.py
+++ so.py
@@ -2,20 +2,28 @@ import requests
 import sys
 from color import *
 from parser import Parser
+import argparse
+
+parser = argparse.ArgumentParser(description='Search Stack Overflow.')
+parser.add_argument('query', type=str, nargs='+', help='keywords to search in the archive')
+parser.add_argument('-l', '--limit', type=int, help='max number of results to return')
+
+args = parser.parse_args()
+
+max_results = 5
+if args.limit:
+    max_results = int(args.limit)
 
 webpage = requests.get("https://stackoverflow.com/questions").text
 
-args = sys.argv
-args.remove('so.py')
-if len(args) > 0:
-    query = '+'.join(args)
-    webpage = requests.get('https://stackoverflow.com/search?q=' + query).text
+if args.query:
+    webpage = requests.get('https://stackoverflow.com/search?q=' + '+'.join(args.query)).text
 
 
 parser = Parser(webpage)
-count = 0
+count = 1
 for question in parser.getSummaries():
-    if count > 6:
+    if count > max_results:
         continue
     title = parser.getTitle(question)
     votes = color(parser.getVotes(question), Color.yellow)
