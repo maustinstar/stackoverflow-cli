efe63769f1d8703300b1119fb11ba894f051aef3
maustinstar <maustinstar@gmail.com>
2020-04-08 13:00:27 -0500

    add dependencies to setup

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     version = "0.1",
     packages = find_packages(),
 
+    install_requires = [
+        "requests>=2.23.0",
+        "beautifulsoup4>=4.9.0",
+    ],
+
     entry_points = {
         "console_scripts": [
             "stackoverflow = stackoverflow.so.py",
